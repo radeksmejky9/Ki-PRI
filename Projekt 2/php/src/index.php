4626a4fe9d45ea814c15b3ff6801f3d2ab1ce81c
radeksmejky9 <71386098+radeksmejky9@users.noreply.github.com>
2024-02-27 17:11:48 +0100

    Projekt 2

--- Projekt 2/php/src/index.php
+++ Projekt 2/php/src/index.php
@@ -55,18 +55,13 @@
     function validate($xmlPath, $xsdPath = '')
     {
         $doc = new DOMDocument;
-
-        // proběhne kontrola well-formed
         libxml_use_internal_errors(true);
         $doc->loadXML(file_get_contents($xmlPath));
         printErrors();
         libxml_use_internal_errors(false);
 
         $isValid = false;
-        // Máme XSD?
         if ($xsdPath) {
-            echo '<p>Validuji podle XSD.';
-            // validace
             libxml_use_internal_errors(true);
             $isValid = $doc->schemaValidate($xsdPath);
             printErrors();
@@ -75,17 +70,13 @@
 
         return $isValid;
     }
-
-    // poslané soubory
     $xmlFile = @$_FILES['xml'];
     $xsdFile = @$_FILES['xsd'];
-
-    // Máme XML?
     if (@$xmlTmpName = $xmlFile['tmp_name']) {
-        $xsdTmpName = $xsdFile['tmp_name'];
-        $isValid = validate($xmlTmpName, $xsdTmpName);
-        if ($isValid)
-            echo "Nahraný XML soubor je validní.";
+            $xsdTmpName = $xsdFile['tmp_name'];
+            $isValid = validate($xmlTmpName, $xsdTmpName);
+            if ($isValid)
+                echo "Nahraný XML soubor je validní.";
     }
     ?>
 </body>
